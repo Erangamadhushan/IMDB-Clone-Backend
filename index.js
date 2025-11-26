cadabf1dd95e0cec21941550563c02f802a49923
Erangamadhushan <erangamadhushann956@gmail.com>
2025-11-27 01:19:31 +0530

    Initial project setup with Express server, environment configuration, and basic routing. Added .gitignore, package.json, and package-lock.json for dependency management.

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const config = require('./config/env.config');
+
+
+const app = express();
+const PORT = config.PORT;
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Basic route work properly",
+        success: true
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server in running on http://localhost:${PORT}`);
+})
