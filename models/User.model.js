9b271201e710423af4857e9f88c1e99797f355c0
Erangamadhushan <erangamadhushann956@gmail.com>
2025-12-15 05:35:05 +0530

    Remove unused user favorite controller, middleware, and routes; add favoriteMovies field to user model

--- models/User.model.js
+++ models/User.model.js
@@ -20,7 +20,13 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
         minlength:[6, 'Password must be at least 6 characters long']
-    }
+    },
+    favoriteMoview: [
+        {
+            type: String,
+            
+        }
+    ]
 }, { timestamps: true});
 
 // userSchema.pre('save', async function(next) {
