665b9b0cad9a2ed1d9ebb62112a81e106e6c6129
Erangamadhushan <erangamadhushann956@gmail.com>
2026-02-08 11:46:21 +0530

    Refactor app structure by moving express setup to app.js and cleaning up index.js

--- index.js
+++ index.js
@@ -1,39 +1,10 @@
-const express = require('express');
-const cors = require('cors');
+const app = require('./app');
 const config = require('./config/env.config');
-const userRoutes = require('./routes/user.routes');
-const connectDB = require('./config/db.config');
-
-
-const app = express();
 const PORT = config.PORT;
-
-app.use(express.json());
-app.use(cors({
-    origin: "https://imdb-clone-sigma-beige.vercel.app/",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}));
-app.use(express.urlencoded({
-    extended: true
-}));
+const connectDB = require('./config/db.config');
 
 // Database Connection
 connectDB();
-
-app.use('/api/auth/user', userRoutes);
-
-
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: "Basic route work properly",
-        success: true
-    })
-});
-
-app.get('/api/health', (req, res) => {
-  res.json({ status: 'ok', message: 'Server is running' });
-});
 
 app.use((req, res, next) => {
     res.status(404).json({
